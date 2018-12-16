4d6aa32ae9746a9c8574b7926ae71171ed231a9d
MONTER <monter@M.local>
2018-12-15 20:22:27 -0600

    IDK

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -13,7 +13,140 @@
 
 
 
+void shoot()
+{
+	motorSet(PMotor, -127);
+	motorSet(PMotor2, -127);
+
+	delay(2000);
+}
+void goForward(int rotDeg)
+{
+	motorStopAll();
+	encoderReset(rightEncoder);
+	encoderReset(leftEncoder);
+
+	if(rotDeg < 0)
+	{
+		while(encoderGet(rightEncoder) > rotDeg && encoderGet(leftEncoder) > rotDeg)
+		{
+				motorSet(RightMot, -127);
+				motorSet(RightMotT, -127);
+				motorSet(Mid2, -127);
+
+				motorSet(LeftMot, -127);
+				motorSet(LeftMotT, -127);
+				motorSet(Mid, -127);
+		}
+	}
+	else
+	{
+		while(encoderGet(rightEncoder) < rotDeg && encoderGet(leftEncoder) < rotDeg)
+		{
+				motorSet(RightMot, 127);
+				motorSet(RightMotT, 127);
+				motorSet(Mid2, 127);
+
+				motorSet(LeftMot, 127);
+				motorSet(LeftMotT, 127);
+				motorSet(Mid, 127);
+		}
+	}
+
+	motorStopAll();
+	encoderReset(rightEncoder);
+	encoderReset(leftEncoder);
+}
+
+void turnRight(int deg)
+{
+	if(deg == 0)
+			deg = 280;
+
+	motorStopAll();
+	encoderReset(rightEncoder);
+	encoderReset(leftEncoder);
+
+	//turn left to aim at the blue flag
+	while(encoderGet(leftEncoder) < deg)
+	{
+			motorSet(RightMot, -127);
+			motorSet(RightMotT, -127);
+			motorSet(Mid2, -127);
+
+			motorSet(LeftMot, 127);
+			motorSet(LeftMotT, 127);
+			motorSet(Mid, 127);
+	}
+
+	motorStopAll();
+	encoderReset(rightEncoder);
+	encoderReset(leftEncoder);
+}
+void turnLeft(int deg)
+{
+	if(deg == 0)
+			deg = 280;
+	motorStopAll();
+	encoderReset(rightEncoder);
+	encoderReset(leftEncoder);
+
+	//turn left to aim at the blue flag
+	while(encoderGet(rightEncoder) < deg)
+	{
+			motorSet(RightMot, 127);
+			motorSet(RightMotT, 127);
+			motorSet(Mid2, 127);
+
+			motorSet(LeftMot, -127);
+			motorSet(LeftMotT, -127);
+			motorSet(Mid, -127);
+	}
+
+	motorStopAll();
+	encoderReset(rightEncoder);
+	encoderReset(leftEncoder);
+}
+void closeRed()
+{
+      //so we can exit the loop when we are done with the period
+      bool isDone = false;
+
+      while(!isDone)
+      {
+      shoot();
+
+      turnLeft(17);
+      //go forward from initial square
+      goForward(1400);
+
+      goForward(-1600);
+
+
+			turnRight(0);
+
+			goForward(2000);
+      motorStopAll();
+      encoderReset(rightEncoder);
+      encoderReset(leftEncoder);
+
+
+      isDone = true;
+
+      //shutdown the encoders, power will be lost and any function calls related to the encoder in the function parameters will be ignored.
+      encoderShutdown(rightEncoder);
+      encoderShutdown(leftEncoder);
+
+    }
+
+}
+
+
+
+
 void operatorControl();
+
+
 
 
 //toggle and press bool for forward motion of drum
